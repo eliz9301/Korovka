35ecfc438bb02184ea45ceb094aefd8b4123861d
bvzgjkz <122781449+bvzgjkz@users.noreply.github.com>
2024-02-27 05:37:46 +0600

    Задание готово к проверке

--- ConsoleApplication16/ConsoleApplication16.cpp
+++ ConsoleApplication16/ConsoleApplication16.cpp
@@ -1,12 +1,19 @@
 ﻿#include <stdio.h>
+
 int main() {
     int x, y;
+
     printf("Введите значение x: ");
     scanf("%d", &x);
 
-    y = 5 * x + 20;
 
-    printf("Значение y равно %d\n", y);
+    y = 5 * x + 20;
+    printf("Значение y: %d\n", y);
+
+
+    y *= 2;
+    printf("Значение у, умноженное на 2: %d\n", y);
+
 
     return 0;
 }
