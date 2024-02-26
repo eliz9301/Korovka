9f5b1710b85459a13d95624eb02651c31df42a8e
bvzgjkz <122781449+bvzgjkz@users.noreply.github.com>
2024-02-27 05:36:27 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApplication16/ConsoleApplication16.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+int main() {
+    int x, y;
+    printf("Введите значение x: ");
+    scanf("%d", &x);
+
+    y = 5 * x + 20;
+
+    printf("Значение y равно %d\n", y);
+
+    return 0;
+}
